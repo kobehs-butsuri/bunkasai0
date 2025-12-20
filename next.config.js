3f92191a98656e809511879377e63d30d3baa8ab
rer <fodosoaodosoaoso@gmail.com>
2025-12-20 10:26:58 +0900

    SVGをファイルからの読み込みに変更

--- next.config.js
+++ next.config.js
@@ -3,7 +3,57 @@
  */
 const nextConfig = {
     output: 'export',
-    trailingSlash: true
+    trailingSlash: true,
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.('.svg'),
+        );
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            svgoConfig: {
+                                plugins: [
+                                    {
+                                        name: 'preset-default',
+                                        params: {
+                                            overrides: {
+                                                cleanupIds: false,
+                                                convertShapeToPath: false,
+                                                collapseGroups: false,
+                                                removeUnknownsAndDefaults: {
+                                                    keepRoleAttr: true,
+                                                },
+                                                removeEmptyContainers: false,
+                                            },
+                                        },
+                                    },
+                                ],
+                            },
+                            prettier: false,
+                            svgProps: {
+                            },
+                        },
+                    },
+                ],
+            },
+        );
+
+        fileLoaderRule.exclude = /\.svg$/i;
+
+        return config;
+    }
 }
 
 module.exports = nextConfig
