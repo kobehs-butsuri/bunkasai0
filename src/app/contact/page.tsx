27b58c987d0d3f1f9a6273fbd5cffe4098e095ef
Kobe High School Physics Club <kobehs.butsuri@gmail.com>
2026-04-20 09:10:45 +0900

    余分なcssを削除

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
         <>
             <iframe
                 src="https://docs.google.com/forms/d/e/1FAIpQLSdRcSirRn90UZA2LBCpRlTq0vpPECDzyGJF4dBJyq4NSR6c3Q/viewform?embedded=true"
-                className="w-full max-w-7xl mx-auto h-[calc(100dvh-8rem)] md:h-[calc(100dvh-5rem)] overflow-auto block"
+                className="w-full max-w-7xl mx-auto h-[calc(100dvh-8rem)] md:h-[calc(100dvh-5rem)] block"
             />
             <Footer/>
         </>
