63a24cb3abe55dda757c79042ada195fe2a626eb
rer <fodosoaodosoaoso@gmail.com>
2026-04-23 20:48:09 +0900

    あいさつ文の修正

--- src/app/greeting/page.tsx
+++ src/app/greeting/page.tsx
@@ -2,7 +2,7 @@ import {Metadata} from "next";
 import Footer from "@/components/footer";
 
 export const metadata : Metadata = {
-    title: "自治会長・文化委員長挨拶",
+    title: "ご挨拶",
 }
 
 export default function Greeting() {
