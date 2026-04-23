5c66808097fe76614f8012e02eeb694b9f77754c
rer <fodosoaodosoaoso@gmail.com>
2026-04-23 20:22:36 +0900

    開発版用の修正

--- src/components/under-construction-checker.tsx
+++ src/components/under-construction-checker.tsx
@@ -14,6 +14,7 @@ const underConstructionPages = [
 ];
 
 function isUnderConstruction(pathname: string): boolean {
+    return false;
     const normalizedPath = pathname.replace(/\/$/, '') || '/';
 
     return underConstructionPages.some(page => {
